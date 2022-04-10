b9eb46691152c77be550718568e46ba5ab54f508
chanshukei <chanshukei417@gmail.com>
2022-04-10 23:46:00 +0100

    2nd version

--- sicaworld-app/Home.js
+++ sicaworld-app/Home.js
@@ -1,13 +1,53 @@
-import { StyleSheet, Button } from 'react-native';
+import {Button, StyledButton, ButtonText} from 'react-native';
+import { Fontisto } from "@expo/vector-icons";
+import * as Google from 'expo-google-app-auth';
+import { useState } from 'react';
+import { ScreenContainer } from 'react-native-screens';
 
 const Home = ({ navigation }) => {
+    const [googleSubmitting, setGoogleSubmitting] = useState(false);
+
+    
+const handleGoogleSignIn = () => {
+    setGoogleSubmitting(true);
+
+    const config = {
+        androidClientId: '941729682379-3jfperv7ae155ttm3k9cs2qlkrd71406.apps.googleusercontent.com',
+        iosClientId: '941729682379-uf3ns9n8udfgou5qhegjtd0gsfd8pie7.apps.googleusercontent.com',
+        scopes: ['profile', 'email'],
+    };
+    
+    Google.logInAsync(config)
+        .then((result) =>{
+            const{type, user} = result;
+            if(type=='success'){
+                const {email, name} = user;
+                console.log('Google signin successful');
+                console.log('Email:'+email+' Name:'+name);
+            }else{
+                console.log('Google signin was cancelled');
+            }
+            setGoogleSubmitting(false);
+        })
+        .catch((error) => {
+            console.log(error);
+            setGoogleSubmitting(false);
+        });
+    }
+
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() =>
-          navigation.navigate('Profile', { name: 'Jane' })
-        }
-      />
+        <ScreenContainer>
+            <Button
+                title="Go to Jane's profile"
+                onPress={() =>
+                    navigation.navigate('Profile', { name: 'Jane' })
+                }
+            />
+            <Button
+                title="Sign in with Google"
+                onPress={handleGoogleSignIn}
+            />
+      </ScreenContainer>
     );
   }
   
